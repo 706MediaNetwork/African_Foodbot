33b758975c6dad11ed82c9d2b276342121ead8c7
706MediaNetwork <706mlab.network@gmail.com>
2024-06-20 05:22:30 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
+from llama_index.llms.openai import OpenAI 
 from llama_index.core import VectorStoreIndex, ServiceContext, Document
-from llama_index.llms import OpenAI
-import openai
 from llama_index import SimpleDirectoryReader
 
 openai.api_key = st.secrets.openai_key
